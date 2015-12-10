ac9fe745a23dcce0ed529b5e736e7251bd4ce403
Keith Miyake <keith.miyake@gmail.com>
2015-12-10 13:49:59 -0500

    add data.people task and update schedule data script

--- gulp-tasks/data.js
+++ gulp-tasks/data.js
@@ -1,85 +1,109 @@
 module.exports = function (request, baby, yaml, fs) {
-  return function (done) {
-    request('https://docs.google.com/spreadsheets/d/16RfbdrnDHhRgP2iZwNw6AVSyWy5VoKn0nB0CpyMa658/pub?gid=585110058&single=true&output=csv',
-    function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-        var csvData = baby.parse(body, {header: true});
-        fs.writeFile('src/_data/workshops.yml',
-          yaml.dump(csvData.data),
-          function (err) {
-            if (err) {
-              return console.log(err);
-            }
-            console.log('Wrote src/_data/workshops.yml');
-          });
-      } else {
-        console.log('Failed to download the workshop data from Google Docs.');
-        console.log('Error: ' + error);
-        console.log('Response: ' + response);
-      }
-    });
-
-    request('https://docs.google.com/spreadsheets/d/16RfbdrnDHhRgP2iZwNw6AVSyWy5VoKn0nB0CpyMa658/pub?gid=0&single=true&output=csv',
-    function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-        var csvData = baby.parse(body, {header: true, skipEmptyLines: true});
-        var rows = csvData.data;
-        var outData = [];
-        for (var i = 0; i < rows.length; i++) {
-          var row = rows[i];
-          if (row.Time.indexOf('Day') > -1) {
-            outData.push({day: row.Time, date: row.Session, timeslots: []});
+  return {
+    workshops: function (done) {
+      request('https://docs.google.com/spreadsheets/d/16RfbdrnDHhRgP2iZwNw6AVSyWy5VoKn0nB0CpyMa658/pub?gid=585110058&single=true&output=csv',
+        function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            var csvData = baby.parse(body, {header: true});
+            fs.writeFile('src/_data/workshops.yml',
+              yaml.dump(csvData.data),
+              function (err) {
+                if (err) {
+                  return console.log(err);
+                }
+                console.log('Wrote src/_data/workshops.yml');
+              });
           } else {
-            if (row.Track1) {
-              outData[outData.length - 1].timeslots.push({
-                time: row.Time,
-                session: row.Session,
-                title: row.Track1,
-                room: row.Room,
-                instructor: row.InstructorsTrack1,
-                link: row.link1,
-                track: 1
-              });
-            }
-            if (row.Track2) {
-              outData[outData.length - 1].timeslots.push({
-                time: row.Time,
-                session: row.Session,
-                title: row.Track2,
-                room: row.Room2,
-                instructor: row.InstructorsTrack2,
-                link: row.link2,
-                track: 2
-              });
-            }
-            if (row.Track1 === '' && row.Track2 === '') {
-              outData[outData.length - 1].timeslots.push({
-                time: row.Time,
-                session: row.Session,
-                room: row.Room,
-                track: 0
-              });
-            }
+            console.log('Failed to download the workshop data from Google Docs.');
+            console.log('Error: ' + error);
+            console.log('Response: ' + response);
           }
-        }
+        });
+      done();
+    },
 
-        fs.writeFile('src/_data/schedule.yml',
-          yaml.dump(outData),
-          function (err) {
-            if (err) {
-              return console.log(err);
+    people: function (done) {
+      request('https://docs.google.com/spreadsheets/d/16RfbdrnDHhRgP2iZwNw6AVSyWy5VoKn0nB0CpyMa658/pub?gid=1411565774&single=true&output=csv',
+        function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            var csvData = baby.parse(body, {header: true, skipEmptyLines: true, comments: '//'});
+            fs.writeFile('src/_data/people.yml',
+              yaml.dump(csvData.data),
+              function (err) {
+                if (err) {
+                  return console.log(err);
+                }
+                console.log('Wrote src/_data/people.yml');
+              });
+          } else {
+            console.log('Failed to download the workshop data from Google Docs.');
+            console.log('Error: ' + error);
+            console.log('Response: ' + response);
+          }
+        });
+      done();
+    },
+
+    schedule: function (done) {
+      request('https://docs.google.com/spreadsheets/d/16RfbdrnDHhRgP2iZwNw6AVSyWy5VoKn0nB0CpyMa658/pub?gid=0&single=true&output=csv',
+        function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            var csvData = baby.parse(body, {header: true, skipEmptyLines: true});
+            var rows = csvData.data;
+            var outData = [];
+            for (var i = 0; i < rows.length; i++) {
+              var row = rows[i];
+              if (row.Time.indexOf('Day') > -1) {
+                outData.push({day: row.Time, date: row.Session, timeslots: []});
+              } else {
+                if (row.Track1 && row.Session) {
+                  outData[outData.length - 1].timeslots.push({
+                    time: row.Time,
+                    session: row.Session,
+                    title: row.Track1,
+                    room: row.Room,
+                    instructor: row.InstructorsTrack1,
+                    link: row.link1,
+                    track: 1
+                  });
+                }
+                if (row.Track2 && row.Session) {
+                  outData[outData.length - 1].timeslots.push({
+                    time: row.Time,
+                    session: row.Session,
+                    title: row.Track2,
+                    room: row.Room2,
+                    instructor: row.InstructorsTrack2,
+                    link: row.link2,
+                    track: 2
+                  });
+                }
+                if (!row.Session) {
+                  outData[outData.length - 1].timeslots.push({
+                    time: row.Time,
+                    title: row.Track1,
+                    room: row.Room,
+                    track: 0
+                  });
+                }
+              }
             }
-            console.log('Wrote src/_data/schedule.yml');
-          });
-      } else {
-        console.log('Failed to download the schedule data from Google Docs.');
-        console.log('Error: ' + error);
-        console.log('Response: ' + response);
-      }
-    });
 
-    // shell.exec('python scripts/csvyml/schedule.py src/_data/schedule.yml');
-    // shell.exec('python scripts/csvyml/workshops.py src/_data/workshops.yml');
-    done();
+            fs.writeFile('src/_data/schedule.yml',
+              yaml.dump(outData),
+              function (err) {
+                if (err) {
+                  return console.log(err);
+                }
+                console.log('Wrote src/_data/schedule.yml');
+              });
+          } else {
+            console.log('Failed to download the schedule data from Google Docs.');
+            console.log('Error: ' + error);
+            console.log('Response: ' + response);
+          }
+        });
+      done();
+    }
   };
 };
