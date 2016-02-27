de9b94072c2768dfbf9aca20760652e56ed0417d
Keith Miyake <keith.miyake@gmail.com>
2016-02-27 02:32:07 -0500

    fix and clean up npm build system

--- build_vendor.js
+++ build_vendor.js
@@ -1,37 +1,40 @@
-var buildify = require('buildify');
+const buildify = require('buildify');
+const fs = require('fs-extra');
 
-buildify.task({
-  name: 'js',
-  task: function() {
-    console.log('building vendor javascript');
-    buildify('bower_components')
-      .concat([
-          'jquery/dist/jquery.min.js',
-          'animated-header/js/animated-header.js',
-          'FitText.js/jquery.fittext.js',
-          'jquery.easing/js/jquery.easing.min.js',
-          'wow/dist/wow.min.js',
-          'tether/dist/js/tether.js',
-          'bootstrap/dist/js/bootstrap.js',
-          'jquery.serializeJSON/jquery.serializejson.min.js',
-          'bootstrap-validator/dist/validator.min.js'])
-      .uglify()
-      .save('../assets/javascript/vendor.min.js');
+// Build vendor javascript
+buildify('bower_components')
+  .concat([
+    'jquery/dist/jquery.min.js',
+    'animated-header/js/animated-header.js',
+    'FitText.js/jquery.fittext.js',
+    'jquery.easing/js/jquery.easing.min.js',
+    'wow/dist/wow.min.js',
+    'tether/dist/js/tether.js',
+    'bootstrap/dist/js/bootstrap.js',
+    'jquery.serializeJSON/jquery.serializejson.min.js',
+    'bootstrap-validator/dist/validator.min.js'])
+  .uglify()
+  .save('../js/vendor.min.js');
+
+// Build vendor styles
+buildify('bower_components')
+  .concat([
+    'animate.css/animate.min.css',
+    'font-awesome/css/font-awesome.min.css',
+    'font-mfizz/css/font-mfizz.css'
+  ])
+  .cssmin()
+  .save('../css/vendor.min.css');
+
+// Copy vendor fonts
+fs.copy(
+  'bower_components/font-awesome/fonts',
+  'bower_components/font-mfizz/fonts',
+  'font',
+  function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    return console.log('copied fonts');
   }
-});
-
-buildify.task({
-  name: 'css',
-  task: function() {
-    console.log('building vendor css');
-    buildify('bower_components')
-      .concat([
-          'animate.css/animate.min.css',
-          'font-awesome/css/font-awesome.min.css',
-          'font-mfizz/css/font-mfizz.css'
-      ])
-      .cssmin()
-      .save('../assets/css/vendor.min.css');
-  }
-});
-
+);
